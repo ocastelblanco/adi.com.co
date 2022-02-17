ac445cee1e71930a625a50b1f9c9e1fb3bb9da98
ocastelblanco <ocastelblanco@gmail.com>
2022-02-17 17:39:30 -0500

    Inclusión de HTML en el JSON de estructura

--- src/app/servicios/estructura.service.ts
+++ src/app/servicios/estructura.service.ts
@@ -24,7 +24,7 @@ export class EstructuraService {
   private estructura: BehaviorSubject<Estructura[]> = new BehaviorSubject<Estructura[]>([]);
   constructor(private http: HttpClient) { }
   init(): void {
-    const url: string = 'https://script.google.com/macros/s/AKfycbzg2TvO0mp2otz8e9tnjg8X3K2Fn5asT8cJotRsBX-HLlzc3qvyQlkymeYnROsSAkrp/exec';
+    const url: string = 'https://script.google.com/macros/s/AKfycbzXCVU1WXUlFgWtS8U5ngaYPEvjLqLgKjUH8XXTFOSUdhH_EtxEWGpPDEj2zU3RwHsx/exec';
     this.http.get(url, { responseType: 'json' }).subscribe((data: any) => this.estructura.next(data));
   }
   getEstructura(): BehaviorSubject<Estructura[]> {
