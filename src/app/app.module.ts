0d5af5d71ad352387ebd518a6d16a10a7de64405
ocastelblanco <ocastelblanco@gmail.com>
2022-06-15 18:24:53 -0500

    chore: updating ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ClarityModule } from '@clr/angular';
 import { NgxJsonLdModule } from '@ngx-lite/json-ld';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from "ng-recaptcha";
 
 import { ContenedorComponent } from './contenedor/contenedor.component';
 import { HeaderComponent } from './vistas/header/header.component';
@@ -37,13 +38,18 @@ import { ErrorInterceptor } from './interceptadores/error.interceptor';
     FormsModule,
     ReactiveFormsModule,
     NgxJsonLdModule,
-    NgxSkeletonLoaderModule
+    NgxSkeletonLoaderModule,
+    RecaptchaV3Module
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
       multi: true
+    },
+    {
+      provide: RECAPTCHA_V3_SITE_KEY,
+      useValue: '6LdE31ogAAAAACHZF0rq29cPFbJyylQbrNIAesaW'
     }
   ],
   bootstrap: [AppComponent]
--- src/app/vistas/contactenos/contactenos.component.ts
+++ src/app/vistas/contactenos/contactenos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EstructuraService, Mensaje } from 'src/app/servicios/estructura.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
   selector: 'adi-contactenos',
@@ -30,9 +31,9 @@ export class ContactenosComponent implements OnInit {
     nombre: new FormControl('', [Validators.required, Validators.minLength(5)]),
     telefono: new FormControl('', [Validators.pattern('[0-9\+ ]*'), Validators.minLength(10)]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    mensaje: new FormControl('', [Validators.required])
+    mensaje: new FormControl('', [Validators.required]),
   });
-  constructor(private data: EstructuraService) { }
+  constructor(private data: EstructuraService, private recaptchaV3Service: ReCaptchaV3Service) { }
   ngOnInit(): void {
     this.schema = {
       '@context': 'http://schema.org',
@@ -43,27 +44,36 @@ export class ContactenosComponent implements OnInit {
     };
   }
   enviarMensaje() {
-    this.mensaje.nombre = this.contacto.get('nombre')?.value;
-    this.mensaje.email = this.contacto.get('email')?.value;
-    this.mensaje.telefono = this.contacto.get('telefono')?.value;
-    this.mensaje.mensaje = this.contacto.get('mensaje')?.value;
-    this.data.putMensaje(this.mensaje)
+    this.alertaMensaje = 'info';
+    this.recaptchaV3Service.execute('contactenos')
       .subscribe({
-        next: (resp: any) => {
-          this.mensaje = { nombre: '', telefono: '', email: '', mensaje: '' };
-          this.contacto.reset();
-          if (resp.error === null) {
-            this.alertaMensaje = 'success';
-          } else {
-            this.alertaMensaje = 'danger';
-          }
+        next: (token: string) => {
+          this.mensaje.nombre = this.contacto.get('nombre')?.value;
+          this.mensaje.email = this.contacto.get('email')?.value;
+          this.mensaje.telefono = this.contacto.get('telefono')?.value;
+          this.mensaje.mensaje = this.contacto.get('mensaje')?.value;
+          this.data.putMensaje(this.mensaje)
+            .subscribe({
+              next: (resp: any) => {
+                this.mensaje = { nombre: '', telefono: '', email: '', mensaje: '' };
+                this.contacto.reset();
+                if (resp.error === null) {
+                  this.alertaMensaje = 'success';
+                } else {
+                  this.alertaMensaje = 'danger';
+                }
+              },
+              error: (err: any) => {
+                console.log('Error de CORS, pero no es incapacitante', err);
+                this.mensaje = { nombre: '', telefono: '', email: '', mensaje: '' };
+                this.contacto.reset();
+                this.alertaMensaje = 'success';
+              }
+            });
         },
-        error: (err: any) => {
-          console.log('Error de CORS, pero no es incapacitante', err);
-          this.mensaje = { nombre: '', telefono: '', email: '', mensaje: '' };
-          this.contacto.reset();
-          this.alertaMensaje = 'success';
-        }
+        error: (error: any) => {
+          this.alertaMensaje = 'danger';
+        },
       });
   }
 }
